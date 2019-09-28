cbdf0f8ade77e870bfee8e563d0f57fcbe04f5cb
github iwbc-mzk <iwbc_mzk@yahoo.co.jp>
2019-09-29 00:33:36 +0900

    add index view

--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from portfolio import views as portfolio_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', portfolio_view.IndexView.as_view())
 ]
