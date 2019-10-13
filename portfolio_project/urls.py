64b5367e673f9fb53228b72a166d5575180c5039
github iwbc-mzk <iwbc_mzk@yahoo.co.jp>
2019-10-14 06:56:18 +0900

    fix: viewの呼び出しをプロジェクトのURLファイルからアプリのURLファイルからに変更

--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from portfolio import views as portfolio_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('portfolio/', portfolio_view.PortfolioView.as_view())
+    path('portfolio/', include('portfolio.urls'))
 ]
