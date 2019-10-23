525f74040204e4e622108fc3477cb46172483bc6
mizuki iwabuchi <m-iwabuchi-biz@f04.itscom.net>
2019-10-23 23:03:08 +0900

    Fix: メニューリンククリック時に、メニュー色変更アニメーションが正常に動作しない問題の修正

--- portfolio/static/portfolio/js/portfolio.js
+++ portfolio/static/portfolio/js/portfolio.js
@@ -1,20 +1,22 @@
 $( function() {
     $("a.nav-link, a.navbar-brand").click( function () {
-        var hrefValue = jQuery(this).attr( 'href' );
-        $(hrefValue).animatescroll({padding:55});
+        var hrefValue = $(this).attr('href');
+        $(hrefValue).animatescroll({scrollSpeed:1000, padding:55}, 1000);
 
         $(this).addClass('active');
 
         $('.navbar-toggler').attr('aria-expanded', 'false').addClass('collapsed');
         $('.navbar-collapse').collapse('hide');
+        return false;
     });
 
-    window.addEventListener( 'scroll', () => {
+    $(window).on("scroll", function(){
         var scrollTop = $(window).scrollTop();
+        var windowHeight = $(window).height();
         if(scrollTop > 70){
             $('#navbarTop').removeClass('navbar-dark').addClass('navbar-light').addClass('menu-shadow').addClass('menu-scrolled', 600);
         }else{
             $('#navbarTop').removeClass('navbar-light').addClass('navbar-dark').removeClass('menu-shadow').removeClass('menu-scrolled', 600);
         }
-    }, false);
+    })
 })
