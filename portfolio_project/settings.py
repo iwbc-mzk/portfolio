e64e07429479043db2db18fa3c6b95ac7c31f88d
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2022-07-03 13:23:13 +0900

    change network structure

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['bibliophage.jp']
+ALLOWED_HOSTS = ['portfolio.bibliophage.jp']
 
 
 # Application definition
